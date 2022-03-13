ae616138028517f8cb4445df3cceb09176674f96
Md Hasib <md.hasibuzzaman001@google.com>
2022-03-13 23:27:11 +0600

    get foods from foods collections

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ltnk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xx1x.mongodb.net/hungry-kitchen?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,12 +23,23 @@ async function run() {
     await client.connect();
     console.log("database connected successfully");
     await client.connect();
-    const database = client.db("hungry_kitchen");
+    const database = client.db("hungry-kitchen");
     const foodsCollection = database.collection("foods");
 
+    // //GET Foots API
+    app.get("/foods", async (req, res) => {
+      const cursor = foodsCollection.find({});
+      const foods = await cursor.toArray();
+      res.send(foods);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello From Newspaper database!");
+  res.send("Hello From Hungry Kitchen database!");
 });
 
 app.listen(port, () => {
