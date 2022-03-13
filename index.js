2f4ad0de714bb7992b8424c828dd8a4654953ac4
Md Hasib <md.hasibuzzaman001@google.com>
2022-03-14 00:00:28 +0600

    role based query complete

--- index.js
+++ index.js
@@ -25,12 +25,20 @@ async function run() {
     await client.connect();
     const database = client.db("hungry-kitchen");
     const foodsCollection = database.collection("foods");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
 
-    // //GET Foots API
+    //Foods Collectios--------------------
     app.get("/foods", async (req, res) => {
       const cursor = foodsCollection.find({});
       const foods = await cursor.toArray();
       res.send(foods);
+    });
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
+      res.json(result);
     });
     app.get("/category", async (req, res) => {
       const category = req.query.category;
@@ -38,6 +46,25 @@ async function run() {
       const cursor = foodsCollection.find(query);
       const categories = await cursor.toArray();
       res.json(categories);
+    });
+    // Orders collections ---------
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    // users collections ---------
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+    app.get("/role", async (req, res) => {
+      const role = req.query.role;
+      const query = { role: role };
+      const cursor = usersCollection.find(query);
+      const orles = await cursor.toArray();
+      res.json(orles);
     });
   } finally {
     // await client.close();
