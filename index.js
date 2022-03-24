795fb3d2d19dfaf542c890cf8657eef3bed1f4bc
wjobayer <creativewebdeveloperjobayer@gmail.com>
2022-03-24 19:37:35 +0600

    payment intent

--- index.js
+++ index.js
@@ -76,13 +76,13 @@ async function run() {
       const orders = await cursor.toArray();
       res.send(orders);
     });
-    app.get("/orders/:email", async (req, res) => {
+    app.get("/orders/pay/:email", async (req, res) => {
       const result = await ordersCollection
        .find({ userEmail: req.params.email })
        .toArray();
        res.send(result);
 });
-    app.get('/orders/payment/:id', async (req, res) => {
+    app.get('/orders/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await ordersCollection.findOne(query);
